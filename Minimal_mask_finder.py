31f42362134010dcf1fce5cc9c13686ec4c5d6d3
Verderf <Desolace-1224>
2020-06-06 01:48:11 +0300

    Made finding-the-mask loops a little bit less messy

--- Minimal_mask_finder.py
+++ Minimal_mask_finder.py
@@ -13,15 +13,17 @@ def decimal_formatter(ip):                                                      
 def mask_generator(input_array):
     binary_ip_array = [decimal_formatter(x) for x in input_array]                                            # LET'S CREATE A LIST TO FEED TO THE MASK FINDING FUNCTION
     #print(binary_ip_array)
-    fdb_array = []
-    for i in range(len(binary_ip_array)):                                                                    # FINDING FIRST DIFFERENT BIT IN THE IP ARRAY TURNED INTO BINARY
-        for j in range(len(binary_ip_array)):                                                                # TO GENERATE THE MINIMAL SUBNET MASK
-            for bit in range(len(binary_ip_array[i])):
-                for another_bit in range(len(binary_ip_array[j])):
-                    if binary_ip_array[i][bit] != binary_ip_array[j][bit]:
-                        fdb_array.append(bit)
-    first_different_bit = min(fdb_array)
-    mask_binary = ('1' * (first_different_bit)).ljust(32, '0')                                               # HERE'S THE BINARY REPRESENTATION OF MASK
+    last_common_bit = 32                                                                                     # NOW FIND THE LAST COMMON BIT IN ALL THE IP ADRESSES PROVIDED, THIS'LL BE THE MASK
+    for ip in binary_ip_array:
+        for ip2 in binary_ip_array:
+            bit_num = 0
+            for compare_bit1, compare_bit2 in zip(ip, ip2):
+                if compare_bit1 != compare_bit2:
+                    if bit_num < last_common_bit : last_common_bit = bit_num
+                    break
+                bit_num += 1
+
+    mask_binary = ('1' * (last_common_bit)).ljust(32, '0')                                               # HERE'S THE BINARY REPRESENTATION OF MASK
     assert len(mask_binary) == 32, 'Binary mask is to be a 32 bit string as well'
 
     ip_octets = [binaryToDec(binary_ip_array[0][i:i + 8]) for i in range(0, len(binary_ip_array[0]), 8)]     # NOW LET'S BREAK IN DOWN INTO OCTETS AND CONVERT BACK TO DECIMAL
@@ -31,7 +33,7 @@ def mask_generator(input_array):
 
     #print(ip_octets, mask_octets)
 
-    print('Minimum mask length for the given ip list is ' + str(first_different_bit) + ' bits. \nBinary mask : ' + str(mask_binary))
+    print('Minimum mask length for the given ip list is ' + str(last_common_bit) + ' bits. \nBinary mask : ' + str(mask_binary))
     print('Network address is : ' + str(subnet_ip).replace(',','.').strip('[]'))
 
     return mask_binary
@@ -39,7 +41,6 @@ def mask_generator(input_array):
 
 ###############################################################################################################################################################################
 
-ip_list = ['192.168.2.25', '192.168.5.68', '192.168.10.15', '192.168.15.254']                                # FEEL FREE TO TEST IT WITH DIFFERENT IP SETS
-
+#ip_list = ['192.168.2.25', '192.168.5.68', '192.168.10.15', '192.168.15.254']                                # FEEL FREE TO TEST IT WITH DIFFERENT IP SETS
+ip_list = ['172.16.55.33', '172.16.55.44', '172.16.85.12', '172.16.14.22']
 mask_generator(ip_list)
-
