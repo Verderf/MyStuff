a4f02a05dd6d981657ed39bc515525668e3f432c
Verderf <Desolace-1224>
2020-12-04 22:08:35 +0300

    primary working script gen added

--- drilling_marker.py
+++ drilling_marker.py
@@ -16,11 +16,12 @@ detail_measures = []
 vertical_array = []
 horizontal_array = []
 
-detail_measures.append([float(item) for item in input('Введите размер детали X Y Z через пробел > ').split()])  # TODO: never used. We gonna use this in horizonal drilling.
+detail_measures.append([float(item) for item in input('Введите размер детали X Y Z через пробел > ').split()])
 
+print(detail_measures)
 
 print("""\rВЕРТИКАЛЬНОЕ СВЕРЛЕНИЕ.
-Введите координаты через пробел. (x   y   l   T)
+Введите координаты точки через пробел. (x   y   l   T)
 x, y  - координаты
 l - глубина сверления
 T - номер инструмента
@@ -32,7 +33,7 @@ Esc - закончить и сформировать инструкцию
 vertical_array.append([float(item) for item in input('Введите переменные > ').split()])
 
 with open('instruction.txt', 'w') as file:
-    file.write("M6T1\rG43\rM3 S6000\r")
+    file.write("\r")
 
 while True:
 
@@ -46,7 +47,8 @@ while True:
 
         with open('instruction.txt', 'a') as file:
             for coord_line in vertical_array:
-                file.write(f"\rG0 X{coord_line[0]:.4f} Y{coord_line[1]:.4f} Z30.0000\r"
+                file.write(f"M6T{int(coord_line[3])}\rG43\rM3 S6000\r"
+                           f"\rG0 X{coord_line[0]:.4f} Y{coord_line[1]:.4f} Z30.0000\r"
                            f"  Z18.0000\r"
                            f"G1 Z0.0000 F240\r"
                            f"G0 Z30.0000\r")
@@ -57,15 +59,16 @@ while True:
 
 yes = ['yes', 'ye', 'y', 'д', 'да']
 
-horizontal_prompt = input('Добавить горизонтальное сверление?  д/н  y/n')
+horizontal_prompt = input('Добавить горизонтальное сверление?  д/н  y/n ')
 
 if horizontal_prompt in yes:
 
     print("""\rГОРИЗОНТАЛЬНОЕ СВЕРЛЕНИЕ.
-    Введите координаты через пробел. (x   y   l   T)
-    x, y  - координаты
+    Введите координаты точки через пробел. (x   y   z   l   T   S)
+    x, y, z  - координаты
     l - глубина сверления
     T - номер инструмента
+    S - сторона
     Enter - добавить новое отверстие
     Esc - закончить и сформировать инструкцию
      \r""")
@@ -73,12 +76,32 @@ if horizontal_prompt in yes:
     horizontal_array.append([float(item) for item in input('Введите переменные > ').split()])
 
     while True:
+
         print('Задать еще одно отверстие - Enter, закончить - Esc')
         key_press = msvcrt.getch()
 
         if ord(key_press) == 13:
-            vertical_array.append([float(item) for item in input('Переменные > ').split()])
+            horizontal_array.append([float(item) for item in input('Переменные > ').split()])
         elif ord(key_press) == 27:
             print(f'Создаем инструкцию.. \r{vertical_array}')
+
+            with open('instruction.txt', 'a') as file:
+                for coord_line in horizontal_array:
+                    file.write(f"*****\rM6T{int(coord_line[4])}\rG43\rM3 S6000\r"
+                               f"\rG0 X{coord_line[0]:.4f} Y{coord_line[1]:.4f} Z30.0000\r"
+                               f"Z{coord_line[2]:.4f}\r")
+
+                    if coord_line[-1] == 1:
+                        ex = 0 + coord_line[3]
+                        file.write(f"Y{ex:.4f} F240\r"
+                                   f"Y0.0000\r")
+                    else:                                                                   # TODO: do the exceptions on third side
+                        ex = detail_measures[0][1] - coord_line[3]
+                        file.write(f"Y{ex:.4f} F240\r"
+                                   f"Y{detail_measures[0][1]:.4f}\r")
+
+                    file.write(f"Z30.0000\r")
+
+            break
 
 print('\rГотово.')
